e3ab319b4f181f9289efcdd91754d4b6b8e6ae35
phung001 <phung001@ucr.edu>
2015-06-01 20:23:50 -0700

    merged and adjusted some code

---
+++ main.c
@@ -0,0 +1,538 @@
+#include <avr/io.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include "./path/io.c"
+#include "./path/io.h"
+
+//-------------------------------------------------------------------------------------
+// macro for easier usage
+#define read_eeprom_word(address) eeprom_read_word ((const uint16_t*)address)
+#define write_eeprom_word(address,value) eeprom_write_word ((uint16_t*)address,(uint16_t)value)
+#define update_eeprom_word(address,value) eeprom_update_word ((uint16_t*)address,(uint16_t)value)
+
+//declare an eeprom array
+unsigned char EEMEM  my_eeprom_array;
+
+// declare a ram array
+unsigned char my_ram_array;
+
+//----------------------------------------------------------------------------------------
+
+double notes[16] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 
+					523.25, 587.33, 659.25, 698.46, 783.99, 880.00, 987.77, 1046.50, 0};
+
+unsigned char buttons[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+	
+double song1[50] = { ,1.23}; //1 terminated for each song
+double song2[50] = { ,1.23};
+double song3[50] = { ,1.23};
+double song4[50] = { ,1.23};
+
+
+//--------Task scheduler data structure--------------------
+// Struct for Tasks represent a running process in our
+// simple real-time operating system.
+/*Tasks should have members that include: state, period, a
+measurement of elapsed time, and a function pointer.*/
+typedef struct _task {
+	//Task's current state, period, and the time elapsed
+	// since the last tick
+	signed char state;
+	unsigned long int period;
+	unsigned long int elapsedTime;
+	//Task tick function
+	int (*TickFct)(int);
+} task;
+task tasks[5];
+const unsigned char numTasks=2;
+const unsigned long GCD=25;
+
+enum keypad_states{SM_1};
+unsigned char key;
+int tick_getkeys(int state)
+{
+	key=GetKeypadKey();
+	return state;
+}
+
+enum States{dairy};
+int tick_scroll(int state)
+{
+	LCD_Cursor(1);
+	if(key) {
+		LCD_WriteData(key);
+		my_ram_array++;
+		update_eeprom_word(&my_eeprom_array, my_ram_array);
+	}
+	LCD_Cursor(2);
+	my_ram_array = read_eeprom_word(&my_eeprom_array);
+	LCD_WriteData(my_ram_array);
+	
+	return state;
+				
+}	
+//-----------------------------------------------------------------------------------------------
+
+static char menu;
+static char cursor;
+static char button_hand;
+static char button_shake;
+
+static char button_layout[8];
+static double notes[16];
+static double song1[];
+static double song2[];
+static double song3[];
+static double song4[];
+
+//-----------------------------------------------------------------------------------------------
+enum Keypad_States { Keypad_init, Keypad_wait, Keypad_down, Keypad_up, Keypad_enter/back } Keypad_State;
+
+int Keypad_movement(int state) {
+   
+   switch(state) { // Transitions
+      case -1:
+         state = Keypad_init;
+         break;
+      case Keypad_init:
+         if (1) {
+            state = Keypad_wait;
+         }
+         break;
+      case Keypad_wait:
+         if (keypad == 8 && cursor > 0) {
+            state = Keypad_up;
+         }
+         else if (keypad == 5 && cursor < 3) {
+            state = Keypad_down;
+         }
+         else if (keypad == 2 || keypad == 0) {
+            state = Keypad_enter/back;
+         }
+	 else{
+            state = Keypad_wait;
+         }
+         break;
+      case Keypad_down:
+         if (1) {
+            state = Keypad_wait;
+         }
+         break;
+      case Keypad_up:
+         if (1) {
+            state = Keypad_wait;
+         }
+         break;
+      case Keypad_enter/back:
+         if (1) {
+            state = Keypad_wait;
+         }
+         break;
+      default:
+         state = Keypad_init;
+      } // Transitions
+
+   switch(state) { // State actions
+      case Keypad_init:
+         break;
+      case Keypad_wait:
+         break;
+      case Keypad_down:
+         cursor++;
+         break;
+      case Keypad_up:
+         cursor --;
+         break;
+      case Keypad_enter/back:
+         cursor = 0;
+         break;
+      default: // ADD default behaviour below
+         break;
+   } // State actions
+}
+
+//-----------------------------------------------------------------------------------------------
+enum Menu_States { Menu_init, Menu_button_wait, Menu_main_wait, Menu_record_wait, Menu_play_wait, Menu_reset_wait, Menu_reset, Menu_song1_play, Menu_song2_play, Menu_song3_play, Menu_song4_play, Menu_chang_button_wait, Menu_song1_record, Menu_song2_record, Menu_song3_record, Menu_song4_record } Menu_State;
+int Menu(int states) {
+
+	string main_disp[5];
+	string song_disp[5];
+
+   switch(state) { // Transitions
+      case -1:
+         state = Menu_init;
+         break;
+      case Menu_init:
+         if (1) {
+            state = Menu_main_wait;
+         }
+         break;
+      case Menu_button_wait:
+         if (keypad != 2 //else) {
+            state = Menu_button_wait;
+         }
+         else if (keypad == 0) {
+            state = Menu_main_wait;
+         }
+         else if (keypad == 2 //enter) {
+            state = Menu_chang_button_wait;
+         }
+         break;
+      case Menu_main_wait:
+         if (cursor == 2 && keypad == 2) {
+            state = Menu_button_wait;
+         }
+         else if (cursor == 1 && keypad == 2) {
+            state = Menu_record_wait;
+         }
+         else if (cursor == 0 && keypad == 2) {
+            state = Menu_play_wait;
+         }
+         else if (cursor == 3 && keypad == 2) {
+            state = Menu_reset_wait;
+         }
+         else{
+            state = Menu_main_wait;
+         }
+         break;
+      case Menu_record_wait:
+         if (keypad == 0) {
+            state = Menu_main_wait;
+         }
+         else if (cursor == 0 && keypad == 2) {
+            state = Menu_song1_record;
+         }
+         else if (cursor == 1 && keypad == 2) {
+            state = Menu_song2_record;
+         }
+         else if (cursor == 2 && keypad == 2) {
+            state = Menu_song3_record;
+         }
+         else if (cursor == 3 && keypad == 2) {
+            state = Menu_song4_record;
+         }
+         else{
+            state = Menu_record_wait;
+         }
+         break;
+      case Menu_play_wait:
+         if (keypad == 0) {
+            state = Menu_main_wait;
+         }
+         else if (cursor == 0 && keypad == 2) {
+            state = Menu_song1_play;
+         }
+         else if (cursor == 1 && keypad == 2) {
+            state = Menu_song2_play;
+         }
+         else if (cursor == 2 && keypad == 2) {
+            state = Menu_song3_play;
+         }
+         else if (cursor == 3 && keypad == 2) {
+            state = Menu_song4_play;
+         }
+	 else{
+            state = Menu_play_wait;
+         }
+         break;
+      case Menu_reset_wait:
+         if (keypad == 0) {
+            state = Menu_main_wait;
+         }
+         else if (keypad == 2) {
+            state = Menu_reset;
+         }
+         else{
+            state = Menu_reset_wait;
+         }
+         break;
+      case Menu_reset:
+         if (1) {
+            state = Menu_main_wait;
+         }
+         break;
+      case Menu_song1_play:
+         if (keypad == 0) {
+            state = Menu_play_wait;
+         }
+         break;
+      case Menu_song2_play:
+         if (keypad == 0) {
+            state = Menu_play_wait;
+         }
+         break;
+      case Menu_song3_play:
+         if (keypad == 0) {
+            state = Menu_play_wait;
+         }
+         break;
+      case Menu_song4_play:
+         if (keypad == 0) {
+            state = Menu_play_wait;
+         }
+         break;
+      case Menu_chang_button_wait:
+         if (button_shake == 0) {
+            state = Menu_button_wait;
+         }
+         else if (button_shake != 0;) {
+            state = Menu_chang_button_wait;
+         }
+         break;
+      case Menu_song1_record:
+         if (keypad == 0) {
+            state = Menu_record_wait;
+         }
+         break;
+      case Menu_song2_record:
+         if (keypad == 0) {
+            state = Menu_record_wait;
+         }
+         break;
+      case Menu_song3_record:
+         if (keypad == 0) {
+            state = Menu_record_wait;
+         }
+         break;
+      case Menu_song4_record:
+         if (keypad == 0) {
+            state = Menu_record_wait;
+         }
+         break;
+      default:
+         state = Menu_init;
+      } // Transitions
+
+   switch(state) { // State actions
+      case Menu_init:
+         menu = 0;
+         break;
+      case Menu_button_wait:
+         menu = 3;
+         button_hand = 0;
+         display(button instructions)
+         break;
+      case Menu_main_wait:
+         menu = 0;
+         display(main_disp[cursor]);
+         break;
+      case Menu_record_wait:
+         menu = 2;
+         display(song_disp[cursor])
+         break;
+      case Menu_play_wait:
+         menu = 1;
+         display(song_disp[cursor])
+         break;
+      case Menu_reset_wait:
+         menu = 4;
+         display(are you sure)
+         break;
+      case Menu_reset:
+         //do the reset
+         break;
+      case Menu_song1_play:
+         //play the song
+         display(song name)
+         break;
+      case Menu_song2_play:
+         //play the song
+         display(song name)
+         break;
+      case Menu_song3_play:
+         //play the song
+         display(song name)
+         break;
+      case Menu_song4_play:
+         //play the song
+         display(song name)
+         break;
+      case Menu_chang_button_wait:
+         //do the chang_button state machine
+         button_hand = 1;
+         break;
+      case Menu_song1_record:
+         break;
+      case Menu_song2_record:
+         break;
+      case Menu_song3_record:
+         break;
+      case Menu_song4_record:
+         break;
+      default: // ADD default behaviour below
+         break;
+   } // State actions
+}
+
+//-----------------------------------------------------------------------------------------------
+enum Button_States { Button_init, Button_wait, Button_change, Button_note_up, Button_note_down } Button_State;
+int button_change(int state) {
+
+	static char cap_but; //holds the note[] position of the button
+	static char position; //holds the button_layout[] position of the button
+
+   switch(state) { // Transitions
+      case -1:
+         state = Button_init;
+         break;
+      case Button_init:
+         if (1) {
+            state = Button_wait;
+         }
+         break;
+      case Button_wait:
+         if ((PinC6 || PinC7) && button_hand == 1) {
+		state = Button_change;
+		button_shake = 1;
+		position = posi(key);
+		cap_but = button_layout[position];
+         }
+         else if (keypad == 0) {
+            state = Button_wait;
+            button_shake = 0;
+         }
+         else{
+            state = Button_wait;
+         }
+         break;
+      case Button_change:
+         if (keypad == 8 && cap_but < 16) {
+            state = Button_note_up;
+            cap_but++;
+         }
+         else if (keypad == 5 && cap_but > 0) {
+            state = Button_note_down;
+            cap_but--;
+         }
+         else if (keypad == 0) {
+            state = Button_wait;
+            PWM(0);
+button_shake = 0;
+         }
+         else if (keypad == 2) {
+            Button_State = Button_wait;
+            PWM(0);
+button_layout[position] = cap_but;
+button_shake = 0;
+         }
+	 else{
+	    state = Button_change;
+	 }
+         break;
+      case Button_note_up:
+         if (keypad != 8) {
+            state = Button_change;
+         }
+         else if (keypad == 8) {
+            state = Button_note_up;
+         }
+         break;
+      case Button_note_down:
+         if (keypad != 5) {
+            state = Button_change;
+         }
+         else if (keypad == 5) {
+            state = Button_note_down;
+         }
+         break;
+      default:
+         state = Button_init;
+      } // Transitions
+
+   switch(state) { // State actions
+      case Button_init:
+         button_shake = 0;
+         break;
+      case Button_wait:
+         break;
+      case Button_change:
+         PWM(notes[cap_but])
+         break;
+      case Button_note_up:
+         break;
+      case Button_note_down:
+         break;
+      default: // ADD default behaviour below
+         break;
+   } // State actions
+}
+
+int main() {
+   B = 0; //Init outputs
+   TimerSet(1000);
+   TimerOn();
+   Keypad_State = -1;
+   Menu_State = -1;
+   Button_State = -1;
+   while(1) {
+      TickFct_Keypad_movement();
+      TickFct_Menu();
+      TickFct_button_change();
+      while (!TimerFlag);
+      TimerFlag = 0;
+   }
+}
+
+
+
+//-----------------------------------------------------------------------------------------------
+
+	
+// --------END User defined FSMs---------------------------// Implement scheduler code from PES.
+int main(void)
+{
+	unsigned char i=0;
+	tasks[i].state = SM_1;
+	tasks[i].period = 1;
+	tasks[i].elapsedTime = 1;
+	tasks[i].TickFct = &tick_getkeys;
+	++i;
+				
+	tasks[i].state = dairy;
+	tasks[i].period = 1;
+	tasks[i].elapsedTime = 1;
+	tasks[i].TickFct = &tick_scroll;
+	++i;
+				
+	tasks[i].state = dairy;
+	tasks[i].period = 1;
+	tasks[i].elapsedTime = 1;
+	tasks[i].TickFct = &tick_scroll;
+				
+	DDRA = 0xFF; PORTA = 0x00; // LCD
+	//DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00; // LCD
+	DDRC = 0xF0; PORTC = 0x0F; //keypad
+	LCD_init();
+	// Starting at position 1 on the LCD screen
+				
+	PWM_on();
+	TimerOn();
+				
+	TimerSet(GCD);
+	TimerOn();
+	
+	LCD_Cursor(2);
+	my_ram_array = read_eeprom_word(&my_eeprom_array);
+	LCD_WriteData(my_ram_array);
+	
+	while(1)
+	{
+		for (i = 0; i < numTasks; i++ ) {
+			// Task is ready to tick
+			if ( tasks[i].elapsedTime ==
+			tasks[i].period ) {
+				// Setting next state for task
+				tasks[i].state =
+				tasks[i].TickFct(tasks[i].state);
+				// Reset elapsed time for next tick.
+				tasks[i].elapsedTime = 0;
+			}
+			tasks[i].elapsedTime += 1;
+		}
+		
+		while(!TimerFlag);
+		TimerFlag = 0;
+	}
+}
