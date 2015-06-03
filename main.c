2c376cc7c0c2b55675c5642ad1c42b8ce5cc3ca9
Peter <Peter@ucrwpa2-2-172-88.bulk.ucr.edu>
2015-06-03 12:40:14 -0700

    stuff

--- main.c
+++ main.c
@@ -12,18 +12,23 @@
 #define update_eeprom_word(address,value) eeprom_update_word ((uint16_t*)address,(uint16_t)value)
 
 //declare an eeprom array
-unsigned char EEMEM  my_eeprom_array;
-
-// declare a ram array
-unsigned char my_ram_array;
+double EEMEM eeprom_song1[50];
+double EEMEM eeprom_song2[50];
+double EEMEM eeprom_song3[50];
+double EEMEM eeprom_song4[50];
+unsigned char EEMEM eeprom_buttons[8];
+unsigned char EEMEM eeprom_check;
 
 //----------------------------------------------------------------------------------------
 
 double notes[16] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88,
 523.25, 587.33, 659.25, 698.46, 783.99, 880.00, 987.77, 1046.50, 0};
 
+unsigned char* button_notes[16] = {"C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "PB5", "C6", "00"};
+
 unsigned char buttons[8] = {0, 1, 2, 3, 4, 5, 6, 7};
-	
+
+unsigned char check = 0;
 unsigned char button_hand;
 unsigned char button_shake;
 unsigned char cursor = '0';
@@ -60,22 +65,7 @@ int tick_getkeys(int state)
 	return state;
 }
 
-/*enum States{dairy};
-int tick_scroll(int state)
-{
-	LCD_Cursor(1);
-	if(key) {
-		LCD_WriteData(key);
-		my_ram_array++;
-		update_eeprom_word(&my_eeprom_array, my_ram_array);
-	}
-	LCD_Cursor(2);
-	my_ram_array = read_eeprom_word(&my_eeprom_array);
-	LCD_WriteData(my_ram_array);
-	
-	return state;
-	
-}*/
+
 //-----------------------------------------------------------------------------------------------
 
 /*
@@ -176,8 +166,8 @@ int Keypad_movement(int state) {
 enum Menu_States { Menu_init, Menu_button_wait, Menu_main_wait, Menu_record_wait, Menu_play_wait, Menu_reset_wait, Menu_reset, Menu_song1_play, Menu_song2_play, Menu_song3_play, Menu_song4_play, Menu_chang_button_wait, Menu_song1_record, Menu_song2_record, Menu_song3_record, Menu_song4_record } Menu_State;
 int Menu(int state1) {
 
-//	string main_disp[5];
-//	string song_disp[5];
+	const char*  main_disp[4] = {"-Play Songs     Record Songs", "-Record Songs   Change Button", "-Change Button  Reset All", "-Reset All"};
+	const char*  song_disp[4] = {"-Song #1        Song #2", "-Song #2        Song #3", "-Song #3        Song #4", "-Song #4"};
 
 	switch(state1) { // Transitions
 		case -1:
@@ -334,67 +324,64 @@ int Menu(int state1) {
 
 	switch(state1) { // State actions
 		case Menu_init:
-		LCD_DisplayString(1, "button menu section");
 		break;
 		case Menu_button_wait:
 		button_hand = 0;
-		LCD_DisplayString(1, "button menu section");
+		LCD_DisplayString(1, "Press enter to  change a button");
 		break;
 		case Menu_main_wait:
-		LCD_DisplayString(1, "main menu section");
+		LCD_DisplayString(1, main_disp[cursor]);
 		break;
 		case Menu_record_wait:
-		LCD_DisplayString(1, "record menu section");
+		LCD_DisplayString(1, song_disp[cursor]);
 		break;
 		case Menu_play_wait:
-		LCD_DisplayString(1, "play menu section");
+		LCD_DisplayString(1, song_disp[cursor]);
 		break;
 		case Menu_reset_wait:
-		LCD_DisplayString(1, "reset menu section");
+		LCD_DisplayString(1, "Press ENTER to  reset or go back");
 		break;
 		case Menu_reset:
-		LCD_DisplayString(1, "reset go section");
 		//do the reset
 		break;
 		case Menu_song1_play:
 		//play the song
-		LCD_DisplayString(1, "play song1");
+		LCD_DisplayString(1, "Now playing:     Song 1");
 		break;
 		case Menu_song2_play:
 		//play the song
-		LCD_DisplayString(1, "play song2");
+		LCD_DisplayString(1, "Now playing:     Song 2");
 		break;
 		case Menu_song3_play:
 		//play the song
-		LCD_DisplayString(1, "play song3");
+		LCD_DisplayString(1, "Now playing:     Song 3");
 		break;
 		case Menu_song4_play:
 		//play the song
-		LCD_DisplayString(1, "play song4");
+		LCD_DisplayString(1, "Now playing:     Song 4");
 		break;
 		case Menu_chang_button_wait:
 		//do the chang_button state machine
-		LCD_DisplayString(1, "change button go");
 		button_hand = 1;
 		break;
 		case Menu_song1_record:
-		LCD_DisplayString(1, "record song1");
+		LCD_DisplayString(1, "Now recording:   Song 1");
 		break;
 		case Menu_song2_record:
-		LCD_DisplayString(1, "record song2");
+		LCD_DisplayString(1, "Now recording:   Song 1");
 		break;
 		case Menu_song3_record:
-		LCD_DisplayString(1, "record song3");
+		LCD_DisplayString(1, "Now recording:   Song 1");
 		break;
 		case Menu_song4_record:
-		LCD_DisplayString(1, "record song4");
+		LCD_DisplayString(1, "Now recording:   Song 1");
 		break;
 		default: // ADD default behaviour below
 		break;
 	} // State actions
 	return state1;
 } 
-/*
+
 //-----------------------------------------------------------------------------------------------
 enum Button_States { Button_init, Button_wait, Button_change, Button_note_up, Button_note_down } Button_State;
 int button_change(int state) {
@@ -418,7 +405,7 @@ int button_change(int state) {
 			position = button_position(position);
 			cap_but = note_position(position, buttons);
 		}
-		else if (key == 0) {
+		else if (key == '0') {
 			state = Button_wait;
 			button_shake = 0;
 		}
@@ -427,23 +414,24 @@ int button_change(int state) {
 		}
 		break;
 		case Button_change:
-		if (key == 8 && cap_but < 16) {
+		if (key == '8' && cap_but > 0) {
 			state = Button_note_up;
-			cap_but++;
-		}
-		else if (key == 5 && cap_but > 0) {
-			state = Button_note_down;
 			cap_but--;
 		}
-		else if (key == 0) {
+		else if (key == '5' && cap_but < 15) {
+			state = Button_note_down;
+			cap_but++;
+		}
+		else if (key == '0') {
 			state = Button_wait;
 			set_PWM(0);
 			button_shake = 0;
 		}
-		else if (key == 2) {
+		else if (key == '2') {
 			Button_State = Button_wait;
 			set_PWM(0);
 			buttons[position] = cap_but;
+			update_eeprom_word(&eeprom_buttons[position], cap_but);
 			button_shake = 0;
 		}
 		else{
@@ -451,18 +439,18 @@ int button_change(int state) {
 		}
 		break;
 		case Button_note_up:
-		if (key != 8) {
+		if (key != '8') {
 			state = Button_change;
 		}
-		else if (key == 8) {
+		else if (key == '8') {
 			state = Button_note_up;
 		}
 		break;
 		case Button_note_down:
-		if (key != 5) {
+		if (key != '5') {
 			state = Button_change;
 		}
-		else if (key == 5) {
+		else if (key == '5') {
 			state = Button_note_down;
 		}
 		break;
@@ -475,9 +463,15 @@ int button_change(int state) {
 		button_shake = 0;
 		break;
 		case Button_wait:
+		LCD_DisplayString(1, "Press the buttonto change");
 		break;
 		case Button_change:
-		TimerSet(100);		
+		LCD_DisplayString(1, "Current note:");
+		LCD_Cursor(15);
+		LCD_WriteData(button_notes[cap_but]);
+		LCD_Cursor(16);
+		LCD_WriteData(button_notes[cap_but]);
+
 		set_PWM(notes[cap_but]);
 		while (!TimerFlag){}
 		TimerFlag = 0;
@@ -491,7 +485,7 @@ int button_change(int state) {
 		break;
 	} // State actions	
 	return state;
-} */
+} 
 //-----------------------------------------------------------------------------------------------
 
 // --------END User defined FSMs---------------------------// Implement scheduler code from PES.
@@ -515,30 +509,43 @@ int main(void)
 	tasks[i].elapsedTime = 1;
 	tasks[i].TickFct = &Menu;
 	++i;
-	/*
+
 	tasks[i].state = Button_init;
 	tasks[i].period = 4;
 	tasks[i].elapsedTime = 1;
 	tasks[i].TickFct = &button_change;
-	*/
+
 	DDRA = 0xFF; PORTA = 0x00; // LCD
 	//DDRB = 0xFF; PORTB = 0x00;
 	DDRD = 0xFF; PORTD = 0x00; // LCD
 	DDRC = 0xF0; PORTC = 0x0F; //keypad
 	LCD_init();
 	// Starting at position 1 on the LCD screen
-	//LCD_DisplayString(1, "button menu section");
+	
+	check = read_eeprom_word(&eeprom_check);
+	if(check != 1){
+		update_eeprom_word(&eeprom_check, 1);
+		
+		int j = 0;
+		while(j < 8){
+			buttons[j] = read_eeprom_word(&eeprom_buttons[j]);
+			j++;
+		}
+	}
+	else{
+		int j = 0;
+		while(j < 8){
+			update_eeprom_word(&eeprom_buttons[j], buttons[j]);
+			j++;
+		}
+	}
 	
 	PWM_on();
 	TimerOn();
 	
 	TimerSet(50);
 	TimerOn();
-	/*
-	LCD_Cursor(2);
-	my_ram_array = read_eeprom_word(&my_eeprom_array);
-	LCD_WriteData(my_ram_array);
-	*/
+
 	while(1)
 	{
 		for (i = 0; i < numTasks; i++ ) {
@@ -554,8 +561,6 @@ int main(void)
 			tasks[i].elapsedTime += 1;
 		}
 		
-		LCD_Cursor(31);
-		LCD_WriteData(cursor);
 		LCD_Cursor(32);
 		LCD_WriteData(key);
 		
