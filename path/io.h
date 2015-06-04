627265afbe7b3bcdf559743b35e5ddcd5e4f3410
phung001 <phung001@ucr.edu>
2015-06-03 23:31:13 -0700

    changed to match .c file

--- path/io.h
+++ path/io.h
@@ -5,7 +5,7 @@ void LCD_init();
 void LCD_ClearScreen(void);
 void LCD_WriteCommand (unsigned char Command);
 void LCD_Cursor (unsigned char column);
-void LCD_DisplayString(unsigned char column ,const unsigned char *string);
+void LCD_DisplayString(unsigned char column ,const char *string);
 void delay_ms(int miliSec);
 #endif
 
